a5dc1c9c866775a4de984ba14cef4f34054d3c4a
unknown <monganavishkumar@gmail.com>
2024-08-17 19:33:29 +0530

    added Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Navbar=()=>{
     return(
-        <h2>Mongabuy</h2>
+        <div className='border-b flex bg-transparent '>
+            <Link to='/' className=' italic text-2xl font-sans flex justify-items-start p-4 hover:bg-blue-500'>Mongabuy</Link>
+            <span className='flex-1'></span>
+            <Link to='/login' className=' font-sans font-medium flex justify-center p-4 hover:bg-blue-500'>Login</Link>
+            <Link to='/signup' className=' font-sans font-medium flex justify-center p-4 hover:bg-blue-500'>Signup</Link>
+        </div>
     )
 }
 
